14b995283d96d6574fd83d2138c7aad1d9c41629
Drew Daniels <daniels_drew@protonmail.com>
2022-06-08 10:15:19 -0500

    Normalized post state to decrease number of unnecessary renders.

--- src/features/posts/PostsLists.js
+++ src/features/posts/PostsLists.js
@@ -3,30 +3,37 @@ import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from './TimeAgo'
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { selectAllPosts, fetchPosts } from './postsSlice';
+import { 
+  selectAllPosts, 
+  fetchPosts,
+  selectPostIds,
+  selectPostById
+} from './postsSlice';
 import { Link } from "react-router-dom";
 import { ReactionButtons } from "./ReactionButtons";
 
-const PostExcerpt = ({ post }) => {
-    return (
-      <article className="post-excerpt" key={post.id}>
-        <h3>{post.title}</h3>
-        <div>
-          <PostAuthor userId={post.user} />
-          <TimeAgo timestamp={post.date} />
-        </div>
-        <p className="post-content">{post.content.substring(0, 100)}</p>
-  
-        <ReactionButtons post={post} />
-        <Link to={`/posts/${post.id}`} className="button muted-button">
-          View Post
-        </Link>
-      </article>
-    )
-  }
+let PostExcerpt = ({ postId }) => {
+  const post = useSelector(state => selectPostById(state, postId))
+  return (
+    <article className="post-excerpt" key={post.id}>
+      <h3>{post.title}</h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </div>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+
+      <ReactionButtons post={post} />
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+    </article>
+  )
+}
 
 export const PostsLists = () => {
     const dispatch = useDispatch();
+    const orderedPostIds = useSelector(selectPostIds)
     const posts = useSelector(selectAllPosts);
     const postStatus = useSelector(state => state.posts.status);
     const error = useSelector(state => state.posts.error);
@@ -42,13 +49,8 @@ export const PostsLists = () => {
     if (postStatus === 'loading') {
       content = <Spinner text="Loading..." />
     } else if (postStatus === 'succeeded') {
-      // Sort posts in reverse chronological order by datetime string
-      const orderedPosts = posts
-        .slice()
-        .sort((a, b) => b.date.localeCompare(a.date))
-  
-      content = orderedPosts.map(post => (
-        <PostExcerpt key={post.id} post={post} />
+      content = orderedPostIds.map(postId => (
+        <PostExcerpt key={postId} postId={postId} />
       ))
     } else if (postStatus === 'failed') {
       content = <div>{error}</div>
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,11 +1,19 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { 
+    createEntityAdapter,
+    createSlice, 
+    createAsyncThunk, 
+    createSelector 
+} from '@reduxjs/toolkit';
 import { client } from '../../api/client';
 
-const initialState = {
-    posts: [],
+const postsAdapter = createEntityAdapter({
+    sortComparer: (a, b) => b.date.localeCompare(a.date)
+})
+  
+const initialState = postsAdapter.getInitialState({
     status: 'idle',
-    error: null,
-};
+    error: null
+})
 // THUNKS
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
     const response = await client.get('/fakeApi/posts')
@@ -29,14 +37,14 @@ const postsSlice = createSlice({
     reducers: {
         reactionAdded(state, action) {
             const { postId, reaction } = action.payload;
-            const existingPost = state.posts.find(post => post.id === postId);
+            const existingPost = state.entities[postId];
             if (existingPost) {
                 existingPost.reactions[reaction]++
             }
         },
         postUpdated(state, action) {
             const { id, title, content } = action.payload;
-            const existingPost = state.posts.find(post => post.id === id);
+            const existingPost = state.entities[id]
             if (existingPost) {
                 existingPost.title = title;
                 existingPost.content = content;
@@ -50,26 +58,33 @@ const postsSlice = createSlice({
             })
             .addCase(fetchPosts.fulfilled, (state, action) => {
                 state.status = 'succeeded';
-                state.posts = state.posts.concat(action.payload);
+                // Use the `upsertMany` reducer as a mutating update utility
+                postsAdapter.upsertMany(state, action.payload)
             })
             .addCase(fetchPosts.rejected, (state, action) => {
                 state.status = 'failed';
                 state.error = action.error.message;
             })
-            .addCase(addNewPost.fulfilled, (state, action) => {
-                state.posts.push(action.payload);
-            })
+            // Use the `addOne` reducer for the fulfilled case
+            .addCase(addNewPost.fulfilled, postsAdapter.addOne)
     }
 });
 
 // ACTION CREATORS
 export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions;
 
-// SELECTORS
-// inner 'posts' is the name of the array and outer 'posts' is the name of this slice of state.
-export const selectAllPosts = state => state.posts.posts;
-export const selectPostById = (state, postId) => 
-    state.posts.posts.find(post => post.id === postId);
-
 // 'POSTS' ROOT REDUCER
 export default postsSlice.reducer;
+
+// Export the customized selectors for this adapter using `getSelectors`
+export const {
+    selectAll: selectAllPosts,
+    selectById: selectPostById,
+    selectIds: selectPostIds
+    // Pass in a selector that returns the posts slice of state
+} = postsAdapter.getSelectors(state => state.posts)
+
+export const selectPostsByUser = createSelector(
+    [selectAllPosts, (state, userId) => userId],
+    (posts, userId) => posts.filter(post => post.user === userId)
+)
